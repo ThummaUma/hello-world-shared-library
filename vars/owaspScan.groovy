7ef30fd8220491d8bbee778acbd06e4a607143f2
ThummaUma <thummauma999@gmail.com>
2026-07-30 13:06:58 +0530

    Update owaspScan.groovy

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -1,12 +1,20 @@
 // vars/owaspScan.groovy
 // Runs OWASP Dependency-Check to scan for known vulnerable dependencies
+
 def call(Map config) {
-    dependencyCheck additionalArguments: '''
-        -o "./"
-        -s "./"
-        -f "ALL"
-        --project "hello-world-app"
-    ''', odcInstallation: 'owasp-dependency-check'
+
+    withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+
+        dependencyCheck additionalArguments: """
+            --nvdApiKey ${NVD_API_KEY}
+            -o "./"
+            -s "./"
+            -f "ALL"
+            --project "hello-world-app"
+        """,
+        odcInstallation: 'owasp-dependency-check'
+
+    }
 
     dependencyCheckPublisher pattern: 'dependency-check-report.xml'
 }
