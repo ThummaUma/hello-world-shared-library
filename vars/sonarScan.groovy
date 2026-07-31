45904e85d26188a3cdf391d12a6276ea41550e28
Uma Maheswari Thumma <your-email@example.com>
2026-07-31 10:36:45 +0000

    Exclude generated HTML reports from SonarQube scan

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,12 +1,15 @@
 // vars/sonarScan.groovy
 // Runs SonarQube static analysis on the checked-out code
+
 def call(Map config) {
     def scannerHome = tool 'sonarqube-scanner'
+
     withSonarQubeEnv('sonarqube') {
         sh """
             ${scannerHome}/bin/sonar-scanner \
               -Dsonar.projectKey=${config.projectKey} \
               -Dsonar.sources=. \
+              -Dsonar.exclusions=dependency-check-report.html,**/*.html \
               -Dsonar.host.url=${env.SONAR_HOST_URL} \
               -Dsonar.token=${env.SONAR_AUTH_TOKEN}
         """
